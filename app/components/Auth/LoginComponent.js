648d7582e04585a116c73c84de26f548d199dc46
Macintosh <macintosh@Au-Duong-Van-Hieu-MacBook-Pro.local>
2020-01-22 22:43:11 +0700

    fix key board login

--- app/components/Auth/LoginComponent.js
+++ app/components/Auth/LoginComponent.js
@@ -16,7 +16,9 @@ import {
   AsyncStorage,
   StatusBar,
   StyleSheet,
-  Platform
+  Platform,
+  ScrollView,
+  TextInput,
 } from "react-native";
 import LoginForm from "../Forms/LoginForm/LoginForm";
 import {
@@ -47,7 +49,11 @@ export default class LoginComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      messageError: ""
+      messageError: "",
+      managed_url: "",
+      user: "",
+      password: "",
+      isRemember: true
     }
   }
 
@@ -82,10 +88,10 @@ export default class LoginComponent extends Component {
       const authCache = await getAuthCache();
       Config.userName = authCache.user;
       if (authCache) {
-        changeLoginForm("managed_url", authCache.managed_url);
-        changeLoginForm("user", authCache.user);
-        changeLoginForm("password", "");
-        changeLoginForm("isRemember", true);
+        this.setState({"managed_url": authCache.managed_url});
+        this.setState({"user": authCache.user});
+        this.setState({"password": ""});
+        this.setState({"isRemember": true});
         if (await getObjectToken() != null)
           verifyRequest({
             Par: qs.stringify(authCache)
@@ -93,19 +99,26 @@ export default class LoginComponent extends Component {
       }
     } else {
       const authCache = await getAuthCache();
-      changeLoginForm("managed_url", authCache.managed_url);
-      changeLoginForm("user", authCache.user);
-      changeLoginForm("password", "");
-      changeLoginForm("isRemember", false);
+      this.setState({"managed_url": authCache.managed_url});
+      this.setState({"user": authCache.user});
+      this.setState({"password": ""});
+      this.setState({"isRemember": false});
     }
   };
 
-  onSubmitForm = form => {
+  onSubmitForm = () => {
     const { authorizeRequest, getTokenRequest, changeLoginForm } = this.props;
     // var text = aesEncrypt("cmd=GET_LIST_AS_REQUEST", "XCKD3C1C2Z6B8VCCUFBSW8UPA9AR8VL7")
     // console.log("__haha__", text)
     // var text2 = aesDecrypt("WpfNQNZqwuD3LPonzqYFXIZ8C4/IQMFeOZGg6zogDH6tA38u+D6BagZtWKsEZGPgmu4MrZGM6b41aiXi/FHfqUovQKsACB8OWK3eAh0UyFmd82Y83qqfsSaNANX1W/m2n2reWCEsRGuQJIhm/6oS5PO28S7vVuth5U8p4WTY3kIBw8dO7xJuqnGost+a1T86hPDEPNlP6dolIOxWzNrsLwMJKLs+xKD0nAGI4Z3AF8dC5Vhh0ueY2STavb04o0pl8NiBkBj4SEbxHimBtJIVJhd+A3TQmfN5wiwAXGD8jb5pw+xGxvU4a/HFupCevPVBLkVKypGnwzvHUVDm7zcY3OyeNqTIfQsONN2s4GquJA5m81s4aAwaUDMxm3/ByDJF9xHG+pLDk2CsX1uBcRjO1YBo9EzDY0Lt/VOpNjDcCloLXYW8N2g0GM/INS6jnJRejyVxvJk+nVnmU6WW9j6x5oxn7DklzUk3vc4G5lVxny4Oy5zhewGjt8c/T8MZ41zFB1NwwAy9A8DA9HkcZjHZrsd/bcRSDwpRlUffKV9AV7Y=", "XCKD3C1C2Z6B8VCCUFBSW8UPA9AR8VL7")
     // console.log("__haha__", text2)
+
+    var form ={
+      managed_url: this.state.managed_url,
+      user: this.state.user,
+      password: this.state.password,
+      isRemember: this.state.isRemember
+    }
 
     var isError = false;
     if (form.managed_url === "" || form.managed_url === undefined) {
@@ -123,16 +136,16 @@ export default class LoginComponent extends Component {
           isError = true;
         }
     if (!isError) {
-      let isRemember = Boolean(form.isRemember);
+      let isRemember = form.isRemember;
       Config.userName = { ...form }.user;
       this.cacheMyLogin({ ...form }, isRemember);
       //this.getCache();
       getTokenRequest({ ...form });
       if (!isRemember) {
-        changeLoginForm("managed_url", "");
-        changeLoginForm("user", "");
+        this.setState({"managed_url": ""});
+        this.setState({"user": ""});
       }
-      changeLoginForm("password", "");
+      this.setState({"password": ""});
     }
   };
 
@@ -147,6 +160,8 @@ export default class LoginComponent extends Component {
       submitLoginForm,
     } = this.props;
     return (
+      <ScrollView style={styles.container} keyboardShouldPersistTaps={'handled'}>
+
       <Container>
         <StatusBar backgroundColor={AppColors.primaryColor} />
         <ImageBackground resizeMode='stretch' source={Images.bg_login_top} style={{ width: '100%', justifyContent: 'center', height: screenHeight / 2 }}>
@@ -158,18 +173,64 @@ export default class LoginComponent extends Component {
         <View style={styles.form}>
           <View style={styles.formChild}>
             <View style={{}}>
-              <LoginForm
-                hideSubmitButton={true}
-                hideClearButton={true}
-                onSubmit={(formData) => { this.onSubmitForm(formData) }}
-              // initialValues={{
-              //   managed_url: "cloud-didim.3-pod.com",
-              //   user: "qtsoft",
-              //   password: "qtsoft001##"
-              // }}
-              />
+                <TextInput
+                    onChangeText={managed_url => this.setState({managed_url: managed_url})}
+                    value={this.state.managed_url}
+                    name={'managed_url'}
+                    placeholder={I18n.t('loginUrl')}
+                    keyboardType='default'
+                    autoCapitalize='none'
+                    returnKeyType={"next"}
+                    style={styles.textInput}
+                    ref={(input) => { this.secondTextInput = input; }}
+                    onSubmitEditing={() => { this.secondTextInput.focus(); }}
+                    blurOnSubmit={false}
+                />
+
+                <View style={{height:10}} />
+                
+                <TextInput
+                    onChangeText={user => this.setState({user: user})}
+                    value={this.state.user}
+                    name={'user'}
+                    placeholder={I18n.t('loginUser')}
+                    keyboardType='default'
+                    autoCapitalize='none'
+                    returnKeyType={"next"}
+                    style={styles.textInput}
+                    ref={(input) => { this.secondTextInput = input; }}
+                    onSubmitEditing={() => { this.thirdTextInput.focus(); }}
+                    blurOnSubmit={false}
+                />
+
+                <View style={{height:10}} />
+
+                <TextInput
+                    onChangeText={password => this.setState({password: password})}
+                    value={this.state.password}
+                    name={'password'}
+                    placeholder={I18n.t('loginPassword')}
+                    keyboardType='default'
+                    secureTextEntry={true}
+                    autoCapitalize='none'
+                    returnKeyType={"done"}
+                    style={styles.textInput}
+                    ref={(input) => { this.thirdTextInput = input; }}
+                />
+
+                <View style={{height:10}} />
+
+                <CheckBox
+                    title={I18n.t('loginRemember')}
+                    checkedIcon='dot-circle-o'
+                    uncheckedIcon='circle-o'
+                    containerStyle={{ borderWidth: 0, backgroundColor: 'transparent', marginLeft: 0, marginTop: 0, padding: 0 }}
+                    onPress={() => this.setState({isRemember: !this.state.isRemember})}
+                    checked={this.state.isRemember}
+                    checkedColor='red'
+                /> 
             </View>
-            <Button onPress={submitLoginForm} style={{ width: '100%', borderRadius: 5, marginTop: 10, alignSelf: 'center', justifyContent: 'center', backgroundColor: AppColors.primaryColor }}>
+            <Button onPress={()=> this.onSubmitForm()} style={{ width: '100%', borderRadius: 5, marginTop: 10, alignSelf: 'center', justifyContent: 'center', backgroundColor: AppColors.primaryColor }}>
               <Text style={{ color: 'white', fontSize: 20 }}>{I18n.t('login')}</Text>
             </Button>
             {this.state.messageError.length != "" &&
@@ -182,6 +243,8 @@ export default class LoginComponent extends Component {
           <Text style={{ alignSelf: 'center', marginTop: normalize(30) }}>{I18n.t('appFooter')}</Text>
         </View>
       </Container>
+      </ScrollView>
+
     );
   }
 }
@@ -208,5 +271,12 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.27,
     shadowRadius: 4.65,
     elevation: 6,
+  }, 
+  textInput: {
+    borderColor: '#e2e6ea',
+    borderRadius: 10,
+    borderWidth: 1, 
+    paddingHorizontal: 10,
+    fontSize: normalize(15)
   }
 });
